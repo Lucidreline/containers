d141b8c32bb34a093dd4976cbdce2ce76e59d81c
Manny <casta.ma502@gmail.com>
2024-02-05 00:38:28 -0800

    Started creating a ContainerCard component.

---
+++ src/components/ContainerCard/ContainerCard.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContainerCard = () => {
+    return (
+        <div className="container-card">
+            <h3 className="container-name">Container A</h3>
+            <br />
+            <span>Tools and Outdoors</span>
+
+        </div>
+    )
+}
+
+export default ContainerCard
---
+++ src/pages/Containers/Containers.page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Containers = () => {
+    return (
+        <div className="containers">
+
+        </div>
+    )
+}
+
+export default Containers
