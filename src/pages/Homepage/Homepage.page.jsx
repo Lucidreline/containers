aeea3c7b6b1a766352d3b3e372c965292f616faf
Manny <casta.ma502@gmail.com>
2024-02-05 14:33:00 -0800

    Created a sections folder and moved in the Containers section.

--- src/pages/Homepage/Homepage.page.jsx
+++ src/pages/Homepage/Homepage.page.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Homepage.styles.scss'
 import Nav from '../../components/Nav/Nav.component'
-import Containers from '../Containers/Containers.page'
+import Containers from '../../sections/Containers/Containers.page'
 
 const Homepage = () => {
     return (
         <div className="homepage">
             <Nav />
             <div className="section-wrapper">
-                <Containers />
 
             </div>
         </div>
