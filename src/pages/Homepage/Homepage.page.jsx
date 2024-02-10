1ee9c980a2fa8fbcaa1645bb76800d4eb911c89d
Lucidreline <casta.ma502@gmail.com>
2024-02-09 22:11:11 -0800

    Trying to add conditional rendering to the sections

--- src/pages/Homepage/Homepage.page.jsx
+++ src/pages/Homepage/Homepage.page.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Homepage.styles.scss'
 import Nav from '../../components/Nav/Nav.component'
 import Containers from '../../sections/Containers/Containers.section'
 import Search from '../../sections/Search/Search.section'
 
 const Homepage = () => {
+    const [currentSection, setCurrentSection] = useState('search')
+
+    let section = <Containers/>
+    switch (currentSection) {
+        case 'containers':
+            section = <Containers/>
+            break;
+
+        case 'search':
+            section = <Search/>
+            break;
+
+        default:
+            section = <Containers/>
+            break;
+    }
+
     return (
         <div className="homepage">
             <Nav />
             <div className="section-wrapper">
-                <Search />
+                {section}
             </div>
         </div>
     )
