9972d9eec69b023c58abe6b4a18f654ab65f7d9f
Lucidreline <casta.ma502@gmail.com>
2024-09-17 17:07:48 -0700

    CStill working on moving items.

--- src/handlers/items/move-item-by-ids.mjs
+++ src/handlers/items/move-item-by-ids.mjs
@@ -6,27 +6,40 @@
 // change item's container ids
 
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, GetCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 const client = new DynamoDBClient({});
 let ddbDocClient = DynamoDBDocumentClient.from(client);
 
-if (process.env.AWS_SAM_LOCAL){
+if (process.env.AWS_SAM_LOCAL) {
     ddbDocClient = DynamoDBDocumentClient.from(new DynamoDBClient({
         endpoint: "http://172.19.0.2:8000"
-      }));
+    }));
 }
 
 const itemsTableName = process.env.ITEMS_TABLE_NAME;
 const containersTableName = process.env.CONTAINERS_TABLE_NAME;
 
-const getContainers = (sendingContainerId, recievingContainerId) => {
+const getContainers = async (sendingContainerId, recievingContainerId) => {
+    const sendingContainer = await getContainerById(sendingContainerId)
+    const recievingContainer = await getContainerById(recievingContainerId)
 
+    console.log("sendingContainer")
+
+    if(sendingContainer == undefined || recievingContainer == undefined)
+        return undefined
+
+    
+    return {
+        sendingContainer,
+        recievingContainer
+
+    }
 }
 
 const getItemById = async (itemId) => {
     const params = {
         TableName: itemsTableName,
-        Key: {id: itemId}
+        Key: { id: itemId }
     }
 
     try {
@@ -34,10 +47,10 @@ const getItemById = async (itemId) => {
         const item = data.Item;
         return item
 
-      } catch (err) {
+    } catch (err) {
         console.log("Error when trying to get an item by id:", err);
         return undefined
-      }
+    }
 
 }
 
@@ -53,38 +66,84 @@ const getAllItemsByIdList = (itemIdList) => {
 }
 
 const changeItemContainerId = async (itemList, recievingContainerId) => {
+    console.log("is this even working?", itemList)
     itemList.forEach(async item => {
+        console.log("item container id", item.container)
         item.container = recievingContainerId
+        console.log("new id", item.container)
+
 
         const params = {
-            TableName: containersTableName,
+            TableName: itemsTableName,
             Item: item
         }
 
         try {
             await ddbDocClient.send(new PutCommand(params));
-            console.log("Success - item added or updated", data);
+            console.log("Success - item added or updated");
         } catch (err) {
-            console.log("Error", err.stack);
+            console.log("Error while trying to change container Ids from items:", err.stack);
         }
 
     })
+
+    return itemList
 }
 
-const getSendingContainer = async (sendingContainerId) => {
+const getContainerById = async (containerId) => {
+    var params = {
+        TableName: containersTableName,
+        Key: { id: containerId },
+    };
 
+    try {
+        const data = await ddbDocClient.send(new GetCommand(params));
+        const container = data.Item;
+        return container
+    } catch (err) {
+        console.log(`Error while looking for Container ${containerId}:`, err);
+    }
 }
 
-const getRecievingContainer = async (recievingContainerId) => {
 
+const removeItemIds = async (itemIds, sendingContainer) => {
+    itemIds.forEach(itemId => {
+        const index = sendingContainer.items.indexOf(itemId)
+        if (index > -1) {
+            sendingContainer.items.splice(index, 1)
+        }
+    })
+
+    const params = {
+        TableName: containersTableName,
+        Item: sendingContainer
+    };
+
+    try {
+        await ddbDocClient.send(new PutCommand(params));
+        console.log(`Success - items removed from container ${sendingContainer.id}`);
+    } catch (err) {
+        console.log("Error", err.stack);
+    }
 }
 
-const removeItemIds = async (sendingContainerId) => {
+const addItemIds = async (itemIds, recievingContainer) => {
+    console.log("recieving has", recievingContainer.items, "so adding", itemIds)
+    const combo = recievingContainer.items.concat(itemIds)
+    recievingContainer.items = combo
+    console.log("result ", combo)
 
-}
+    const params = {
+        TableName: containersTableName,
+        Item: recievingContainer
+    };
 
-const addItemIds = async (recievingContainerId) => {
-
+    try {
+        await ddbDocClient.send(new PutCommand(params));
+        console.log(`Success - Added items to container ${recievingContainer.id}`);
+    } catch (err) {
+        console.log("Error", err.stack);
+    }
 }
 
 
@@ -95,32 +154,34 @@ const addItemIds = async (recievingContainerId) => {
 
 
 export const moveItemsByIdsHandler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    throw new Error(`getMethod only accept POST method, you tried: ${event.httpMethod}`);
-  }
- 
-  const body = JSON.parse(event.body);
-  const itemIds = body.items; // payload will need an array called items
-  const sendingContainerId = body.sendingContainer
-  const recievingContainerId = body.recievingContainer
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`getMethod only accept POST method, you tried: ${event.httpMethod}`);
+    }
 
- 
-//   var params = {
-//     TableName : tableName,
-//     Key: { id: id },
-//   };
+    const body = JSON.parse(event.body);
+    const itemIds = body.items; // payload will need an array called items
+    const sendingContainerId = body.sendingContainer
+    const recievingContainerId = body.recievingContainer
 
-//   try {
-//     const data = await ddbDocClient.send(new GetCommand(params));
-//     var item = data.Item;
-//   } catch (err) {
-//     console.log("Error", err);
-//   }
- 
-//   const response = {
-//     statusCode: 200,
-//     body: JSON.stringify(item)
-//   };
- 
-  return response;
+    const containers = await getContainers(sendingContainerId, recievingContainerId)
+    if (containers == undefined)
+        return {
+            statusCode: 404,
+            body: JSON.stringify({msg: "Something is wrong with the container Ids you gave me."})
+        }
+
+    const itemsList = getAllItemsByIdList(itemIds)
+
+    changeItemContainerId(itemsList, containers.recievingContainer)
+
+    removeItemIds(itemIds, containers.sendingContainer)
+
+    addItemIds(itemIds, containers.recievingContainer)
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(containers.recievingContainer)
+      };
+
+    return response;
 }
